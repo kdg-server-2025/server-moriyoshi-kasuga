e9cedaeef18ee8b82c897e6030745ad86efbf105
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2025-05-13 17:09:14 +0900

    feat: add go

---
+++ go/run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+docker build -t test-go --iidfile /tmp/test-go.id .
+
+if [ $? -ne 0 ]; then
+  echo "Docker build failed"
+  exit 1
+fi
+
+id=$(cat /tmp/test-go.id)
+
+docker run --name test-go "$id"
+
+echo "=== Process ended ==="
+
+echo "Stopping the container..."
+docker stop test-go
+
+echo "Removing the container..."
+docker rm test-go
+
+echo "Done cleaning up."
